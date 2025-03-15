168785cdf569247c304ff7cbac8a54238b90a37d
mosttoffa <mostofacba@gmail.com>
2025-03-15 10:56:09 +0600

    Make Login successfully, create popup after successfull, create redirect after successfull

--- Inventory_v1/Controllers/AuthController.cs
+++ Inventory_v1/Controllers/AuthController.cs
@@ -13,5 +13,23 @@ namespace Inventory_v1.Controllers
         {
             return View();
         }
+        [HttpPost]
+        //public ActionResult Login(string txtUsername, string txtPassword)   // Username and Password view connected here from Login design page
+        //{
+        //    ViewBag.Username = txtUsername;       // ViewBag e Username in hoilo
+        //    return View();                      // after Login successful there will be a Login successful popup then if press Ok then it will redirect to Home page
+        //}
+        public ActionResult Login(string txtUsername, string txtPassword)   // Username and Password view connected here from Login design page
+        {
+            //Session["Username"] = txtUsername;              // Session e Username store korlam
+            return Redirect(Url.Action("Index", "Home"));  // Redirect to Home page directly after login
+        }                                                 // We can use any one method from above two methods for Login 
     }
+
+
+
+
+
+     
+
 }
