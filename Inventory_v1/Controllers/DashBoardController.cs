0b7058ca8180c6f648478d3b6abbebabd68b640c
mosttoffa <mostofacba@gmail.com>
2025-04-13 16:05:43 +0600

    Create Modal

--- Inventory_v1/Controllers/DashBoardController.cs
+++ Inventory_v1/Controllers/DashBoardController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Inventory_v1.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,6 +12,25 @@ namespace Inventory_v1.Controllers
         // GET: DashBoard
         public ActionResult Index()
         {
+            BaseEquipment baseEquipment = new BaseEquipment();
+            List<BaseEquipment> lstEquipment = baseEquipment.ListEquipment();
+
+
+            //// Only Laptop product ta dekhaite cai so tr jonno ei condition/code
+            //List<BaseEquipment> lstEquipment_laptop = new List<BaseEquipment>();
+            //foreach (BaseEquipment obj in lstEquipment)
+            //{
+            //    if (obj.EquipmentName.Contains("Laptop"))
+            //    {
+            //        lstEquipment_laptop.Add(obj);
+            //    }
+            //}
+            //ViewBag.lstEquipment = lstEquipment_laptop;
+
+
+
+            ViewBag.lstEquipment = lstEquipment;
+
             return View();
         }
     }
